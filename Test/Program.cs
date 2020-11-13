474af9f152c4c9527b3127c5e013b27cb5eec579
xin liu <xinliuchnsz@gmail.com>
2020-11-13 22:59:52 +0800

    运行测试类

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,6 +6,19 @@ using System.Text;
 namespace Test {
     public class Program {
         static void Main(string[] args) {
+            TestCommunication testObj = new TestCommunication();
+            // 1
+            testObj.Run_TestConnection();
+            // 2
+            testObj.Run_SetParam(0, 0);
+            // 3
+            int value = 0;
+            testObj.Run_GetParam(0, ref value);
+            // 4
+            string devInfo = "";
+            testObj.Run_GetDeviceInfo(ref devInfo);
+            // 5.
+            testObj.Run_SetIDNote(0, new byte[] { 1, 2, 3 } );
         }
     }
 }
